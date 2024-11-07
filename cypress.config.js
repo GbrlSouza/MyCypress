b5d6a474bea044ef0b4fae874b2fdc2b65ac2cc8
Gabriel C. de Souza <gabrielsouza_1909@hotmail.com>
2024-11-07 17:53:31 -0300

    Relatorios (screenshots, videos, xml e html)

--- cypress.config.js
+++ cypress.config.js
@@ -2,11 +2,14 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   reporter: 'cypress-multi-reporters',
+
   reporterOptions: {
     reporterEnabled: 'cypress-mochawesome-reporter, mocha-junit-reporter',
+    
     mochaJunitReporterReporterOptions: {
       mochaFile: 'cypress/reports/junit/results-[hash].xml'
     },
+
     cypressMochawesomeReporterReporterOptions: {
       charts: true,
       reportPageTitle: 'Relatório de testes',
@@ -15,8 +18,11 @@ module.exports = defineConfig({
       saveAllAttempts: false
     }
   },
+
   chromeWebSecurity: false,
+
   e2e: {
+    video: true,
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on)
       // implement node event listeners here
